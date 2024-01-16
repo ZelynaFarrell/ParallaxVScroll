a24f8e0897a84879dd91d1b2e65c05aa3e5cb7cd
Zelyna Farrell <zsillas@yahoo.com>
2024-01-16 12:30:36 -0800

    Initial Commit

---
+++ ParallaxVScroll/ParallaxVScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxVScrollApp.swift
+//  ParallaxVScroll
+//
+//  Created by Zelyna Sillas on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxVScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
